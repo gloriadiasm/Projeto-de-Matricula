f5466ce2c5c1499d9d037d9e857679e684180a5b
PatronusMirror141 <patronusmirror141@gmail.com>
2020-05-27 22:27:22 -0300

    first commit

---
+++ src/main/java/com/unitri/sistemamatricula/model/User.java
@@ -0,0 +1,12 @@
+package com.unitri.sistemamatricula.model;
+
+public class User {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private Address address;
+    private String cpf;
+    private String previousUniversity;
+    private String course;
+    private RegistryDocuments documents;
+}
---
+++ src/test/java/com/unitri/sistemamatricula/SistemaMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitri.sistemamatricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
